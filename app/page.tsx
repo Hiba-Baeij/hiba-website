13b68249bfcd999806a8e8007029a8e9d8f6c481
Hiba_Be <hibabeiaj@gmail.com>
2023-09-12 03:08:22 +0300

    fix page home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
-import Header from './components/page/Header';
+import HeaderOne from './components/page/HeaderOne';
+import HeaderTwo from './components/page/HeaderTwo';
 
 export default function Home() {
   const handleButtonClick = () => {
@@ -7,20 +8,26 @@ export default function Home() {
     console.log('Button clicked!');
   };
   return (
-    <main className="home relative px-32 overflow-hidden flex justify-between items-center h-screen">
-
+    <main className="home relative  overflow-hidden flex justify-center items-center h-screen ">
       <div>
         <Image style={{ position: 'absolute', top: 0, right: '-2rem', width: 'auto' }} width={80} height={33} src="/shapes/zigzags/zigzag-pink.svg" alt="image-head-1" />
-        <Image style={{ position: 'absolute', top: '5rem', left: '40%' }} width={80} height={33} src="/shapes/zigzags/zigzag-pink.svg" alt="image-head-2" />
+        <Image style={{ position: 'absolute', top: '-3rem', left: '30%', width: 'auto' }} width={120} height={0} src="/shapes/lines/line-yellow.svg" alt="image-head-5" />
+        <Image className='md:block hidden' style={{ position: 'absolute', top: '5rem', left: '50%' }} width={80} height={33} src="/shapes/zigzags/zigzag-green.svg" alt="image-head-2" />
         <Image style={{ position: 'absolute', bottom: '72%', left: '-10rem' }} priority width={400} height={400} src="/shapes/circles/circle-red.svg" alt="image-head-3" />
-        <Image style={{ position: 'absolute', bottom: '50%', left: '20%' }} width={50} height={50} src="/shapes/circles/circle-small-yellow.svg" alt="image-head-4" />
-        <Image style={{ position: 'absolute', left: '-3rem', bottom: '14rem', width: 'auto' }} width={120} height={0} src="/shapes/lines/line-purple.svg" alt="image-head-5" />
-        <Image style={{ position: 'absolute', left: '32rem', bottom: '8rem' }} width={80} height={33} src="/shapes/circles/circle-small-blue.svg" alt="image-head-6" />
         <Image style={{ position: 'absolute', bottom: '-2rem', left: '2rem' }} width={120} height={120} src="/shapes/triangles/triangle-pink.svg" alt="image-head-7" />
       </div>
+      <div className=' grid grid-cols-2 gap-[12rem]'>
+        <div className='md:col-span-1 col-span-2'>
+          <HeaderOne />
 
-      <Header />
-      <Image style={{ position: 'absolute', right: '-3rem', top: '30rem' }} priority width={400} height={400} src="/shapes/circles/circle-blue.svg" alt="image-head-8" />
+        </div>
+        <div className='md:col-span-1 col-span-2'>
+          <HeaderTwo />
+
+        </div>
+      </div>
+      <Image className='md:block hidden' style={{ position: 'absolute', right: '-3rem', top: '35rem' }} priority width={400} height={400} src="/shapes/circles/circle-blue.svg" alt="image-head-8" />
+
     </main>
   )
 }
